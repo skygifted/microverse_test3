b05a549c484e527fcaeb31696e951a8ce8dba218
Abdulazeez Ibrahim <48849318+skygifted@users.noreply.github.com>
2022-10-01 08:41:48 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,272 @@
+
+
+const list = document.getElementsByTagName('li')
+const TextCon = document.getElementsByClassName('text-con')
+
+
+list[0].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'block'
+  TextCon[1].style.display = 'none'
+  TextCon[2].style.display = 'none'
+  TextCon[3].style.display = 'none'
+  TextCon[4].style.display = 'none'
+  TextCon[5].style.display = 'none'
+  TextCon[6].style.display = 'none'
+  TextCon[7].style.display = 'none'
+  TextCon[9].style.display = 'none'
+  TextCon[10].style.display = 'none'
+  TextCon[11].style.display = 'none'
+  TextCon[12].style.display = 'none'
+  TextCon[13].style.display = 'none'
+
+    
+})
+list[1].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'none'
+  TextCon[1].style.display = 'block'
+  TextCon[2].style.display = 'none'
+  TextCon[3].style.display = 'none'
+  TextCon[4].style.display = 'none'
+  TextCon[5].style.display = 'none'
+  TextCon[6].style.display = 'none'
+  TextCon[7].style.display = 'none'
+  TextCon[9].style.display = 'none'
+  TextCon[10].style.display = 'none'
+  TextCon[11].style.display = 'none'
+  TextCon[12].style.display = 'none'
+  TextCon[13].style.display = 'none'
+ 
+})
+
+list[2].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'none'
+  TextCon[1].style.display = 'none'
+  TextCon[2].style.display = 'block'
+  TextCon[3].style.display = 'none'
+  TextCon[4].style.display = 'none'
+  TextCon[5].style.display = 'none'
+  TextCon[6].style.display = 'none'
+  TextCon[7].style.display = 'none'
+  TextCon[9].style.display = 'none'
+  TextCon[10].style.display = 'none'
+  TextCon[11].style.display = 'none'
+  TextCon[12].style.display = 'none'
+  TextCon[13].style.display = 'none'
+ 
+})
+
+list[3].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'none'
+  TextCon[1].style.display = 'none'
+  TextCon[2].style.display = 'none'
+  TextCon[3].style.display = 'block'
+  TextCon[4].style.display = 'none'
+  TextCon[5].style.display = 'none'
+  TextCon[6].style.display = 'none'
+  TextCon[7].style.display = 'none'
+  TextCon[9].style.display = 'none'
+  TextCon[10].style.display = 'none'
+  TextCon[11].style.display = 'none'
+  TextCon[12].style.display = 'none'
+  TextCon[13].style.display = 'none'
+ 
+})
+
+
+list[4].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'none'
+  TextCon[1].style.display = 'none'
+  TextCon[2].style.display = 'none'
+  TextCon[3].style.display = 'none'
+  TextCon[4].style.display = 'block'
+  TextCon[5].style.display = 'none'
+  TextCon[6].style.display = 'none'
+  TextCon[7].style.display = 'none'
+  TextCon[9].style.display = 'none'
+  TextCon[10].style.display = 'none'
+  TextCon[11].style.display = 'none'
+  TextCon[12].style.display = 'none'
+  TextCon[13].style.display = 'none'
+ 
+})
+
+
+list[5].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'none'
+  TextCon[1].style.display = 'none'
+  TextCon[2].style.display = 'none'
+  TextCon[3].style.display = 'none'
+  TextCon[4].style.display = 'none'
+  TextCon[5].style.display = 'block'
+  TextCon[6].style.display = 'none'
+  TextCon[7].style.display = 'none'
+  TextCon[9].style.display = 'none'
+  TextCon[10].style.display = 'none'
+  TextCon[11].style.display = 'none'
+  TextCon[12].style.display = 'none'
+  TextCon[13].style.display = 'none'
+  
+})
+
+
+list[6].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'none'
+  TextCon[1].style.display = 'none'
+  TextCon[2].style.display = 'none'
+  TextCon[3].style.display = 'none'
+  TextCon[4].style.display = 'none'
+  TextCon[5].style.display = 'none'
+  TextCon[6].style.display = 'block'
+  TextCon[7].style.display = 'none'
+  TextCon[9].style.display = 'none'
+  TextCon[10].style.display = 'none'
+  TextCon[11].style.display = 'none'
+  TextCon[12].style.display = 'none'
+  TextCon[13].style.display = 'none'
+ 
+})
+
+
+list[7].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'none'
+  TextCon[1].style.display = 'none'
+  TextCon[2].style.display = 'none'
+  TextCon[3].style.display = 'none'
+  TextCon[4].style.display = 'none'
+  TextCon[5].style.display = 'none'
+  TextCon[6].style.display = 'none'
+  TextCon[7].style.display = 'block'
+  TextCon[9].style.display = 'none'
+  TextCon[10].style.display = 'none'
+  TextCon[11].style.display = 'none'
+  TextCon[12].style.display = 'none'
+  TextCon[13].style.display = 'none'
+ 
+})
+
+
+list[8].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'none'
+  TextCon[1].style.display = 'none'
+  TextCon[2].style.display = 'none'
+  TextCon[3].style.display = 'none'
+  TextCon[4].style.display = 'none'
+  TextCon[5].style.display = 'none'
+  TextCon[6].style.display = 'none'
+  TextCon[7].style.display = 'none'
+  TextCon[8].style.display = 'block'
+  TextCon[9].style.display = 'none'
+  TextCon[10].style.display = 'none'
+  TextCon[11].style.display = 'none'
+  TextCon[12].style.display = 'none'
+  TextCon[13].style.display = 'none'
+ 
+})
+
+
+list[9].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'none'
+  TextCon[1].style.display = 'none'
+  TextCon[2].style.display = 'none'
+  TextCon[3].style.display = 'none'
+  TextCon[4].style.display = 'none'
+  TextCon[5].style.display = 'none'
+  TextCon[6].style.display = 'none'
+  TextCon[7].style.display = 'none'
+  TextCon[8].style.display = 'none'
+  TextCon[9].style.display = 'block'
+  TextCon[10].style.display = 'none'
+  TextCon[11].style.display = 'none'
+  TextCon[12].style.display = 'none'
+  TextCon[13].style.display = 'none'
+ 
+})
+
+
+list[10].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'none'
+  TextCon[1].style.display = 'none'
+  TextCon[2].style.display = 'none'
+  TextCon[3].style.display = 'none'
+  TextCon[4].style.display = 'none'
+  TextCon[5].style.display = 'none'
+  TextCon[6].style.display = 'none'
+  TextCon[7].style.display = 'none'
+  TextCon[8].style.display = 'none'
+  TextCon[9].style.display = 'none'
+  TextCon[10].style.display = 'block'
+  TextCon[11].style.display = 'none'
+  TextCon[12].style.display = 'none'
+  TextCon[13].style.display = 'none'
+ 
+})
+
+list[11].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'none'
+  TextCon[1].style.display = 'none'
+  TextCon[2].style.display = 'none'
+  TextCon[3].style.display = 'none'
+  TextCon[4].style.display = 'none'
+  TextCon[5].style.display = 'none'
+  TextCon[6].style.display = 'none'
+  TextCon[7].style.display = 'none'
+  TextCon[8].style.display = 'none'
+  TextCon[9].style.display = 'none'
+  TextCon[10].style.display = 'none'
+  TextCon[11].style.display = 'block'
+  TextCon[12].style.display = 'none'
+  TextCon[13].style.display = 'none'
+ 
+})
+
+
+list[12].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'none'
+  TextCon[1].style.display = 'none'
+  TextCon[2].style.display = 'none'
+  TextCon[3].style.display = 'none'
+  TextCon[4].style.display = 'none'
+  TextCon[5].style.display = 'none'
+  TextCon[6].style.display = 'none'
+  TextCon[7].style.display = 'none'
+  TextCon[8].style.display = 'none'
+  TextCon[9].style.display = 'none'
+  TextCon[10].style.display = 'none'
+  TextCon[11].style.display = 'none'
+  TextCon[12].style.display = 'block'
+  TextCon[13].style.display = 'none'
+ 
+})
+
+
+list[13].addEventListener('click',()=>{
+
+  TextCon[0].style.display = 'none'
+  TextCon[1].style.display = 'none'
+  TextCon[2].style.display = 'none'
+  TextCon[3].style.display = 'none'
+  TextCon[4].style.display = 'none'
+  TextCon[5].style.display = 'none'
+  TextCon[6].style.display = 'none'
+  TextCon[7].style.display = 'none'
+  TextCon[8].style.display = 'none'
+  TextCon[9].style.display = 'none'
+  TextCon[10].style.display = 'none'
+  TextCon[11].style.display = 'none'
+  TextCon[12].style.display = 'none'
+  TextCon[13].style.display = 'block'
+ 
+})
